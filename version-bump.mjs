05898f5940271dd7497ad54cb93a55b78eef00c4
Thiago M. Pinto <tpinto@indeed.com>
2024-07-28 05:31:44 +0100

    version bomp

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,14 +1,92 @@
 import { readFileSync, writeFileSync } from "fs";
+import { join } from "path";
+import { createInterface } from "readline";
+import simpleGit from "simple-git";
 
-const targetVersion = process.env.npm_package_version;
+const git = simpleGit();
 
-// read minAppVersion from manifest.json and bump version to target version
-let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
-const { minAppVersion } = manifest;
-manifest.version = targetVersion;
-writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+const newVersion = process.argv[2];
 
-// update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8"));
-versions[targetVersion] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+if (!newVersion) {
+    console.error('Please provide the new version as an argument');
+    process.exit(1);
+}
+
+const filesToUpdate = [
+    'manifest.json',
+    'package-lock.json',
+    'package.json',
+    'versions.json'
+];
+
+filesToUpdate.forEach(file => {
+    const filePath = join(__dirname, file);
+    const fileContent = JSON.parse(readFileSync(filePath, 'utf8'));
+
+    if (file === 'versions.json') {
+        const lastVersion = Object.keys(fileContent).pop();
+        fileContent[lastVersion] = "0.15.0";
+        fileContent[newVersion] = "0.15.0";
+    } else {
+        fileContent.version = newVersion;
+    }
+
+    writeFileSync(filePath, JSON.stringify(fileContent, null, '\t'), 'utf8');
+});
+
+const askQuestion = (query) => {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    return new Promise(resolve => rl.question(query, ans => {
+        rl.close();
+        resolve(ans);
+    }));
+}
+
+const showChanges = async () => {
+    console.log('The following changes will be committed:');
+    await git.status().then(status => {
+        console.log(status.modified);
+    });
+}
+
+const commitAndTag = async () => {
+    await showChanges();
+
+    const commitConfirmation = await askQuestion('Do you want to commit these changes? (yes/no) ');
+    if (commitConfirmation.toLowerCase() !== 'yes') {
+        console.log('Aborting commit.');
+        process.exit(1);
+    }
+
+    const commitMessage = `New version release ${newVersion}`;
+
+    try {
+        await git.add(filesToUpdate);
+        await git.commit(commitMessage);
+        await git.push('origin', 'master');
+    } catch (err) {
+        console.error('Failed to execute git commit commands', err);
+        process.exit(1);
+    }
+
+    const tagConfirmation = await askQuestion('Do you want to tag this commit? (yes/no) ');
+    if (tagConfirmation.toLowerCase() !== 'yes') {
+        console.log('Aborting tag.');
+        process.exit(1);
+    }
+
+    try {
+        await git.tag(['-a', newVersion, '-m', newVersion]);
+        await git.pushTags('origin');
+        console.log(`Successfully tagged and pushed version ${newVersion}`);
+    } catch (err) {
+        console.error('Failed to execute git tag commands', err);
+        process.exit(1);
+    }
+}
+
+commitAndTag();
